d0f914429852eef3d97ff565f89b563e14b43d1a
Yichen Yang <yichen.yang@flatironschool.com>
2016-05-30 16:45:24 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ quotient = num1/num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(added)
+  added="I'm right and you are wrong!"
+  added
 end
 
-def greeting
-
+def greeting(greeting, name)
+  "#{greeting},#{name}!"
 end
 
-def return_a_value
-
+def return_a_value(word="Nice")
+  word
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(foods="cheese")
+  foods
 end
